905ad28f41e815343a5f4c3abe09644fa0f83fdf
okiki areo <okikiareo@gmail.com>
2024-04-24 21:20:08 +0100

    signin commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Verify from "./pages/verify/verify";
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
+            <Route index element={"Home"} />
             <Route element={<Auth />}>
                 <Route path="signin" element={<SignIn />} />
                 <Route path="signup" element={<SignUp />} />
--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -1,28 +1,40 @@
 import "./signin.css";
 import { Link, Form } from "react-router-dom";
 
+import iconFB from "../../assets/img/img-fb.png";
+import iconGL from "../../assets/img/img-gl.png";
 import iconMail from "../../assets/img/icon-mail.svg";
 import iconHide from "../../assets/img/icon-eye-hide.svg";
 import iconShow from "../../assets/img/icon-eye-show.svg";
+import features from "../../assets/img/illustration-signup.svg";
 
 const SignIn = () => {
+    const iconPass = true ? iconShow : iconHide;
     return (
         <div className="signin">
-            <div className="signin-col">
+            <div className="signin-col signin-place">
+                <h2 className="signin-lead">Sign In!</h2>
+                <div className="signin-link">Don't have an account?&nbsp;
+                    <Link to="#">Sign up</Link>
+                </div>
                 <Form className="signin-form">
                     <div className="signin-field">
                         <label className="signin-label" htmlFor="">
                             Email
                         </label>
-                        <input className="signin-input" type="text" name="" id="" />
-                        <img className="signin-icon" src="" alt="" />
+                        <div className="signin-fwrap">
+                            <input className="signin-input" type="text" name="" id="" />
+                            <img className="signin-icon" src={iconMail} alt="" />
+                        </div>
                     </div>
                     <div className="signin-field">
                         <label className="signin-label" htmlFor="">
                             Password
                         </label>
-                        <input className="signin-input" type="text" name="" id="" />
-                        <img className="signin-icon" src="" alt="" />
+                        <div className="signin-fwrap">
+                            <input className="signin-input" type="text" name="" id="" />
+                            <img className="signin-icon" src={iconPass} alt="" />
+                        </div>
                         <Link className="signin-reset" to="#">
                             Forgot password?
                         </Link>
@@ -31,18 +43,18 @@ const SignIn = () => {
                 </Form>
                 <div className="signin-hline">or</div>
                 <div className="signin-other">
-                    <Link className="signin-olink" to="#">
-                        <img className="signin-oicon" src="" alt="" />
+                    <Link className="signin-olink flex" to="#">
+                        <img className="signin-oicon" src={iconGL} alt="" />
                         Sign in with Google
                     </Link>
-                    <Link className="signin-olink" to="#">
-                        <img className="signin-oicon" src="" alt="" />
+                    <Link className="signin-olink flex" to="#">
+                        <img className="signin-oicon" src={iconFB} alt="" />
                         Sign in with Google
                     </Link>
                 </div>
             </div>
-            <div className="signin-col">
-                <img src="" alt="" />
+            <div className="signin-col signin-image">
+                <img src={features} alt="" />
             </div>
         </div>
     )
