6b817842cdab71123a7d90f84d0d0ff6546d1f74
okiki areo <okikiareo@gmail.com>
2024-05-04 23:07:21 +0100

    Further commit: Reverted to 729f9538352353f13c14cbeccbc1700225d55558

---
+++ app.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const express = require("express");
+const session = require('express-session')
+const bodyParser = require("body-parser");
+const connectDB = require('./db/connectDB')
+const passport = require("passport");
+const nodemailer = require('nodemailer')
+const otpGenerator = require('otp-generator');
+const cookieParser = require('cookie-parser')
+
+const User = require('./model/user')
+
+
+const authRouter = require('./routes/auth')
+const mainRouter = require('./routes/main')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found')
+
+const app = express();
+require('./middleware/passport');
+
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+// app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
+
+app.use(session({
+    secret: process.env.sessionSECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 2592000000 }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+app.use('', mainRouter)
+app.use('', authRouter)
+
+
+// //error handler
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, async () => {
+  //connect DB
+  await connectDB();
+  console.log(`Server is running on port ${port}\n\nhttp://localhost:${port}`);
+});
