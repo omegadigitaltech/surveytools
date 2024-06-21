8e3568b3ba5b922530a3695b696c7250d7ccfeac
okiki areo <okikiareo@gmail.com>
2024-06-21 14:39:44 +0100

    Updated pages

--- src/layout/auth/auth.jsx
+++ src/layout/auth/auth.jsx
@@ -1,21 +1,10 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./auth.css";
 
 import iconFB from "../../assets/img/icon-facebook-48.svg";
 import iconGL from "../../assets/img/icon-google.svg";
-import { useEffect, useState } from "react";
 
 const Auth = () => {
-    let subTitle;
-    let location = useLocation();
-
-    if (location.pathname = "/signup") {
-        subTitle = "Sign up";
-    } else {
-        subTitle = "Sign in";
-    }
-
-    console.log(location)
     return (
         <section className="auth">
             <div className="auth-wrap wrap">
@@ -23,10 +12,7 @@ const Auth = () => {
                     Survey Pro
                     <span className="auth-w1-circ" aria-hidden></span>
                 </h1>
-                <div className="auth-w2-lead">
-                    {subTitle}
-                </div>
-                <div className="auth-w3">
+                {/* <div className="auth-w3">
                     <div className="auth-w3-list">
                         <Link className="auth-w3-link" to="">
                             <img className="auth-w3-icon" src={iconFB} alt="" />
@@ -38,7 +24,7 @@ const Auth = () => {
                     <div className="auth-w3-line">
                         Or continue with
                     </div>
-                </div>
+                </div> */}
                 <Outlet />
             </div>
         </section>
