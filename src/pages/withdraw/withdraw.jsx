139abf697cfdb0a47d062822a37193647e063e99
okiki areo <okikiareo@gmail.com>
2024-09-06 04:10:21 +0100

    /

--- src/pages/withdraw/withdraw.jsx
+++ src/pages/withdraw/withdraw.jsx
@@ -1,9 +1,16 @@
 import {Link, Form} from "react-router-dom";
 import backaro from "../../assets/img/backaro.svg";
 import "./withdraw.css"
+import React, { useState } from 'react';
 
 
 const Withdraw = () => {
+    const [activeButton, setActiveButton] = useState('bank-acct');
+
+    const handleButtonClick = (buttonType) => {
+      setActiveButton(buttonType);
+    };
+
     return(
          <section className="withdraw-section">
 <div className="wrap">
@@ -21,10 +28,14 @@ const Withdraw = () => {
     <div className="withdraw-container">
         <div className="withdraw-type-head">
             <div className="withdraw-type-btn">
-                <button className="bank-acct active-withdraw">
+                <button className={`bank-acct ${activeButton === 'bank-acct' ? 'active-withdraw' : ''}`}
+            onClick={() => handleButtonClick('bank-acct')}
+                >
                     Bank Account
                 </button>
-                <button className="debit-card">
+                <button 
+                 className={`debit-card ${activeButton === 'debit-card' ? 'active-withdraw' : ''}`}
+                 onClick={() => handleButtonClick('debit-card')}>
                     Debit Card
                 </button>
                 </div>
@@ -39,7 +50,30 @@ const Withdraw = () => {
                     <label className="withdraw-label" htmlFor="card-number">
                        <p>Amount</p> 
                     </label>
-                    <input className="withdraw-input" type="number" name="amount" id="amount" />
+                    <input className="amount-input withdraw-input" type="number" name="amount" id="amount" />
+                </fieldset>
+                <fieldset className="withdraw-field">
+                    <label className="withdraw-label" htmlFor="card-number">
+                       <p>Account Number</p> 
+                    </label>
+                    <input className="acct-name-input withdraw-input" type="number" name="acct-name" id="acct-name" />
+                </fieldset>
+                <fieldset className={`bankname-field withdraw-field ${activeButton === 'debit-card' ? 'hide-field' : ''}`}>
+                    <label className="withdraw-label" htmlFor="card-number">
+                       <p>Bank Name</p> 
+                    </label>
+                    <input className="bank-input withdraw-input" type="text" name="bank-name" id="bank-name" />
+                </fieldset>
+                {/* Toggle show for card select */}
+                <fieldset className={`cardnum-field withdraw-field ${activeButton === 'bank-acct' ? 'hide-field' : ''}`}>
+                    <label className="withdraw-label" htmlFor="card-number">
+                       <p>Card Number</p> 
+                    </label>
+                    <input className="card-input withdraw-input" type="number" name="card-num" id="card-num" />
+                </fieldset>
+                
+                <fieldset className="withdraw-field-btn withdraw-field flex">
+                <input type="submit" value="Preview & Withdraw" class="withdraw-btn"/> 
                 </fieldset>
                 </Form>      
                      </div>
