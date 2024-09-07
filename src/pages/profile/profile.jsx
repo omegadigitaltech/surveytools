52263b47878a55b75cf22efd24d44c0039366c06
okiki areo <okikiareo@gmail.com>
2024-09-07 23:51:09 +0100

    profile

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -4,9 +4,12 @@ import okiki from "../../assets/img/okiki.png";
 import profileBg from "../../assets/img/profile-bg.png";
 
 import "./profile.css"
-
+import data from "../../utils/content/data";
 
 const Profile = () =>{
+
+const useInfos = data.userInfos[0]
+
     return(
         <section className="Profile-section">
 <div className="wrap">
@@ -28,12 +31,46 @@ const Profile = () =>{
     <button className="settings-btn"> Settings</button>
     <button className="edit-profile">Edit Profile</button>
 </div>
-
     </div>
+    
     <div className="profile-info">
 <h4>Public Profile</h4>
 <hr />
+<div className="profile-name-field">
+   <h5>Name</h5>
+<div className="name-display info-display">
+    {useInfos.firstName} {useInfos.lastName}
+</div>
+<p>Ensure to use your real name based on your bank account to help make withdrawal easier and faster</p>
+<div className="agesexdept grid">
+   <div className="age-box ">
+   <h5>Age</h5>
+<div className="age-display info-display">
+{useInfos.age}
+
+</div>
+    </div>
+    <div className="gender-box">
+   <h5>Sex</h5>
+<div className="gender-display info-display">
+{useInfos.gender}
+</div>
     </div> 
+    <div className="dept-box">
+   <h5>Department</h5>
+<div className="dept-display info-display">
+{useInfos.department}
+</div>
+    </div> 
+</div>
+<div className="bio-box">
+<h5>Bio</h5>
+<div className="bio-display">
+    {useInfos.userBio}
+</div>
+</div>
+</div>
+   </div> 
 </div>
         </div>
         </div>
---
+++ src/utils/content/data.js
@@ -0,0 +1,24 @@
+const userInfos = [
+    {userId : 11121,
+        firstName : "peter",
+        lastName : "james",
+        email : "peterjames@gmail.com",
+        telephone: "0802176385",
+        gender: "male",
+        age: "18 - 25",
+        department: "english",
+        userPic: "../../assets/img/okiki.png",
+        userBio: "I am an undergradaute hoping my research gets completed with your answers"
+    }
+]
+
+const userSurvey = [
+    {
+        userId : 11121,
+        surveyTitle: "Diet and Excercise",
+        surveyDesc: "This survey is part of a research project to determine the average position of students who are aware of the occupational therapy seesion in OAU, how important their professional courses are to the nation, other opportunities out there waiting for them and the future of they all holds about their courses"
+        
+    }
+    
+]
+export default {userInfos, userSurvey};
