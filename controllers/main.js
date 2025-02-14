9c0acd8f59775ae6070242d3149508b952d1c681
Tesals <akinluaolorunfunminiyi@gmail.com>
2025-02-14 03:39:57 +0100

    fits

--- controllers/main.js
+++ controllers/main.js
@@ -947,7 +947,7 @@ const receivePaymentWebhook = async (req, res) => {
 const mySurveys = async (req, res) => {
   const {userId} = req;
   try {
-    const user = await User.findOne({id: userId}).sort({createdAt: -1});
+    const user = await User.findOne({id: userId})
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
