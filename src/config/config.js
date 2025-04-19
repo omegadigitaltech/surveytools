764534ac04eb5a23a55c1d5e645a0f48f8788e6c
Akinlua <akinluaolorunfunminiyi@gmail.com>
2025-04-19 09:36:44 +0100

    fits

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   // API_URL: "https://nasty-marrissa-surveypro-991645a4.koyeb.app",
   // API_URL: "https://prospective-colette-surveypro-ee1a3b96.staging.koyeb.app"
-  API_URL: "http://localhost:5001",
+  API_URL: "https://surveypro.onrender.com",
   PAYSTACK_PUBLIC_KEY: "pk_test_cd9e617b7d757cfb91cc430b22d648725772b14d"
 };
 
