ff4fae1ac8976aa25a6f1b9d4ee9f84451f69d81
Akinlua <akinluaolorunfunminiyi@gmail.com>
2025-04-26 17:40:09 +0100

    updasted insigths

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   // API_URL: "https://nasty-marrissa-surveypro-991645a4.koyeb.app",
   // API_URL: "https://prospective-colette-surveypro-ee1a3b96.staging.koyeb.app"
+  // API_URL: 'https://surveypro.onrender.com',
   API_URL: `${import.meta.env.VITE_API_URL}`,
   PAYSTACK_PUBLIC_KEY: `${import.meta.env.VITE_PAYSTACK_PUBLIC_KEY}`
 };
