e0afbd802cced9c366017b88704fd95c3a157e0a
okikiareo <okikiareo@gmail.com>
2025-11-25 07:52:00 +0100

    /

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 const config = {
-  // API_URL: import.meta.env.VITE_API_URL || 'https://staging.surveyprotools.com',
-  API_URL: "https://api.surveyprotools.com",
+  API_URL: import.meta.env.VITE_API_URL || 'https://staging.surveyprotools.com',
   // API_URL: 'https://staging.surveyprotools.com',
-  PAYSTACK_PUBLIC_KEY: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY
+  // PAYSTACK_PUBLIC_KEY: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY
+  PAYSTACK_PUBLIC_KEY: "pk_test_183d72c028ee376920f193555ea93ce89f2718d5"
+
 };
 
 export default config;
