a71f2be38c41a3ba622a4f4bc366357c6341e602
Akinlua <akinluaolorunfunminiyi@gmail.com>
2026-06-21 14:23:52 -0500

    hraph analytics

--- python_analytics/app.py
+++ python_analytics/app.py
@@ -280,4 +280,6 @@ async def generate_stats(request: StatsRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    import os
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
