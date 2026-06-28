0c4bbf1f2a7cf478e95901c4b729a4979a0d335e
okikiareo <okikiareo@gmail.com>
2026-06-28 01:39:14 +0100

    deploy err

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const config = {
  // API_URL: import.meta.env.VITE_API_URL || 'https://api.surveyprotools.com',
 //  API_URL: 'https://surveytools-pf0x.onrender.com',
   // API_URL: 'https://staging.surveyprotools.com',
-  API_URL: 'http://localhost:1574',
+  API_URL: import.meta.env.VITE_API_URL,
   PAYSTACK_PUBLIC_KEY: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY
 };
 
